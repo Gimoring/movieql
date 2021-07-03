bbc96e9a78757f0c517d7b00e57f4acaa62804fe
Gimoring <62681451+Gimoring@users.noreply.github.com>
2021-07-03 22:53:15 +0900

    template for how to use resolver, mutation, query

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,13 +1,18 @@
 // resolver -> query를 solve(해결) 하는 것. 데이터를 가져오는 과정은 얘가 담당한다.
-const junpil = {
-  name: "Junpil",
-  age: 27,
-  gender: 'male'
-}
-
+// resolversms graphQL 서버에서 요청을 받는다.
+// graphQL 서버가 쿼리나 뮤테이션의 정의를 발견하면 리솔버를 찾고 해당 함수를 실행한다.
+// 다른 db갈 수 있고  다른 API 갈 수도있고.
+// Mutation은 Database의 상태가 변할 때 사용된다.
+import { getMovies, getById, addMovie, deleteMovie } from './db';
 const resolvers = {
   Query: {
-    person: () => junpil
+    movies: () => getMovies(),
+    //getM번째 인자는 현재 Object를 보내는 Object, 2번 째는 arguments
+    movie: (_, { id }) => getById(id)
+  },
+  Mutation: {
+    addMovie: (_, {name, score}) => addMovie(name, score),
+    deleteMovie: (_, {id}) => deleteMovie(id),
   }
 };
 
